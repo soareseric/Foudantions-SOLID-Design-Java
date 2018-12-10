9cf45165fd4c8ca7072fc57bc8f65d5717985c25
soareseric <contato.eric.sm@gmail.com>
2018-12-10 14:31:45 -0200

    Projeto criado

---
+++ src/br/com/solid/design/main/Main.java
@@ -0,0 +1,87 @@
+package br.com.solid.design.main;
+
+import java.util.Calendar;
+
+public class Main {
+
+	public  class CalculadoraDeSalario{
+
+	    public double calcula(Funcionario funcionario) {
+	        if(DESENVOLVEDOR.equals(funcionario.getCargo())) {
+	            return dezOuVintePorcento(funcionario);
+	        }
+
+	        if(DBA.equals(funcionario.getCargo()) || TESTER.equals(funcionario.getCargo())) {
+	            return quinzeOuVinteCincoPorcento(funcionario);
+	        }
+
+	        throw new RuntimeException("funcionario invalido");
+	    }
+
+	    private  double dezOuVintePorcento(Funcionario funcionario) {
+	        if(funcionario.getSalarioBase() > 3000.0) {
+	            return funcionario.getSalarioBase() * 0.8;
+	        }
+	        else {
+	            return funcionario.getSalarioBase() * 0.9;
+	        }
+	    }
+
+	    private  double quinzeOuVinteCincoPorcento(Funcionario funcionario) {
+	        if(funcionario.getSalarioBase() > 2000.0) {
+	            return funcionario.getSalarioBase() * 0.75;
+	        }
+	        else {
+	            return funcionario.getSalarioBase() * 0.85;
+	        }
+	    }
+
+	}
+
+	public  enum Cargo {
+	    DESENVOLVEDOR,
+	    DBA,
+	    TESTER
+	}
+
+	public  class Funcionario {
+
+	    private int id;
+	    private String nome;
+	    private Cargo cargo;
+	    private Calendar dataDeAdmissao;
+	    private double salarioBase;
+	    public int getId() {
+	        return id;
+	    }
+	    public String getNome() {
+	        return nome;
+	    }
+	    public Cargo getCargo() {
+	        return cargo;
+	    }
+	    public Calendar getDataDeAdmissao() {
+	        return dataDeAdmissao;
+	    }
+	    public double getSalarioBase() {
+	        return salarioBase;
+	    }
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+	    public void setCargo(Cargo cargo) {
+	        this.cargo = cargo;
+	    }
+	    public void setDataDeAdmissao(Calendar dataDeAdmissao) {
+	        this.dataDeAdmissao = dataDeAdmissao;
+	    }
+	    public void setSalarioBase(double salarioBase) {
+	        this.salarioBase = salarioBase;
+	    }
+
+	}
+
+}
